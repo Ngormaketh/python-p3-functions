0c6c6ae0b6ecf44688cb13377cf91858fbcf898e
Anyang Diing <anyang.ngor@student.moringaschool.com>
2025-04-09 20:32:26 +0300

    Python Functions Success

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
-def greet_programmer():
-    pass
+class Greeter:
+    def greet_programmer(self):
+        print("Hello, programmer!")
 
-def greet(name):
-    pass
+    def greet(self, name):
+        print(f"Hello, {name}!")
 
-def greet_with_default(name="programmer"):
-    pass
+    def greet_with_default(self, name="programmer"):
+        print(f"Hello, {name}!")
 
-def add(num1, num2):
-    pass
+    def add(self, num1, num2):
+        return num1 + num2
 
-def halve(number):
-    pass
+    def halve(self, number):
+        if not isinstance(number, (int, float)):
+            return None
+        return number / 2
